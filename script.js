7536206f41c5bdaac29aaf09e1d44ff5eef871de
rinkal singapuri <rinkalsingapuri@gmail.com>
2025-02-25 21:29:15 +0530

    created to-do app

--- script.js
+++ script.js
@@ -583,28 +583,28 @@
 // a = 5;
 
 {
-    function one() {
+    // function one() {
 
-        function two() {
-            console.log(`namaste`);
+    //     function two() {
+    //         console.log(`namaste`);
 
-            function three() {
-                console.log(`hola`);
-                
-            }
+    //         function three() {
+    //             console.log(`hola`);
 
-            three()
-            
-        }
+    //         }
 
-        two()
+    //         three()
 
-        console.log(`hello`);
+    //     }
 
-        
-    }
+    //     two()
 
-    one()
+    //     console.log(`hello`);
+
+
+    // }
+
+    // one()
 }
 
 {
@@ -843,14 +843,14 @@
     // const demo = function() {
     //     const a = 13;
     //     console.log(this);
-        
+
     // }
     // // demo()
 
     // // const demo2 = () => {
     // //     const a = 13;
     // //     console.log(this);
-        
+
     // // }
     // // demo2()
 
@@ -866,12 +866,12 @@
 
     //     // printInfo : function({fname, lname}) {
     //     //     console.log(`first name is ${fname} and last name is ${lname}`);
-            
+
     //     // }
 
     //     printInfo : ({username, name : {fname}}) => {
     //         console.log(`usename is ${username} and fname is ${fname}`);
-            
+
     //     }
     // }
 
@@ -952,7 +952,7 @@
     // })( 5)
 
     // console.log(IIFE);
-    
+
 
 
 
@@ -1291,3 +1291,84 @@
 // }
 
 
+// TO-DO ------------------------------------------------------------------------------------------------------------------
+
+let todo = prompt("Task/Tasks : ");
+todo = todo.split(" ");
+
+const oglist = [...todo];
+
+let req = prompt(" type list/ add/ remove or oglist to perform respected operations");
+
+while (true) {
+    if (req == "quit") {
+        console.log(`the app is closing`);
+        break;
+
+    }
+
+    if (req == "list") {
+        console.log(`The list : ${todo.join(", ")}`);
+        console.log();
+
+        req = prompt("Type list/ add/ remove/ oglist or quit:");
+
+    }
+
+    else if (req == "add") {
+
+        let addTask = prompt("Task/Tasks you want to add : ");
+        addTask = addTask.split(" ")
+
+        console.log(`tasks you want to add : ${addTask.join(", ")}`);
+
+        addTask.forEach((task) => {
+            if (todo.includes(task)) {
+                console.log(`the task : "${task}" you want to add is already in the list.`);
+            } else {
+                todo.push(task);
+            }
+        })
+
+        console.log(`updated list after adding tasks : ${todo.join(", ")}`);
+        console.log();
+
+        req = prompt("Type list/ add/ remove/ oglist or quit:");
+
+
+    }
+
+    else if (req == "remove") {
+
+        let deleteTask = prompt("Task/Tasks you want to delete : ");
+        deleteTask = deleteTask.split(" ")
+
+        console.log(`the tasks you want to delet : ${deleteTask.join(", ")} `);
+
+        deleteTask.forEach((task) => {
+            if (!todo.includes(task)) {
+                console.log(`the task : "${task}" you want to delete is not in the list.`);
+            }
+        })
+
+        todo = todo.filter(task => !deleteTask.includes(task));
+        console.log(`updated list : ${todo.join(", ")}`);
+        console.log();
+
+        req = prompt("Type list/ add/ remove/ oglist or quit:");
+
+
+    }
+
+    else if (req == "oglist") {
+        console.log(`The original list : ${oglist.join(", ")}`);
+
+        req = prompt("Type list/ add/ remove/ oglist or quit:");
+
+    }
+
+    else {
+        req = prompt(`Invalid command, please Type list/ add/ remove/ oglist or quit:`);
+    }
+
+}
