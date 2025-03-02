600d6a686399ed30c47c0a0d84d1d737b3acfacd
rinkal singapuri <rinkalsingapuri@gmail.com>
2025-03-02 23:03:10 +0530

    DOM lenovo to asus

---
+++ DOM.js
@@ -0,0 +1,32 @@
+// function preventSubmit(event) {
+//     event.preventDefault();
+
+//     function printMSG() {
+//         let username = document.getElementById("username").value;
+//         document.getElementById("greet").innerHTML = "<b>hello</b> " + username + "!";
+//     }
+//     printMSG()
+// }
+// let newElement = document.createElement("a");
+// newElement.innerHTML = "google";
+// newElement.setAttribute("href", "https://www.google.com");
+// newElement.setAttribute("target", "_blank");
+// document.body.appendChild(newElement);
+
+
+
+
+
+// function chnagecolor() {
+//     let ele = document.getElementById("target");
+//     ele.classList.toggle("textinterchange");
+//     ele.classList.toggle("text");
+
+// }
+
+
+
+
+
+// let list = document.getElementsByTagName("li")
+// let alllist = document.querySelectorAll("Li")
