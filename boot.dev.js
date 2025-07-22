24f74ab87e2e2cad82ba1d8ae516a303cd7481e8
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-22 22:11:01 +0530

    Some base functional problems.

---
+++ boot.dev.js
@@ -0,0 +1,53 @@
+let campaign = [
+    {
+        camp: "hello",
+        name: "rinkal"
+    },
+    {
+        camp: "hi",
+        name: "krina"
+    },
+    {
+        camp: "namaste",
+        name: "pinki"
+    },
+    {
+        camp: "hola",
+        name: "shilpa"
+    }
+];
+
+campaign.forEach((campa) => {
+    campa.id = campa.camp + "-" + campa.name
+})
+
+console.log(campaign);
+
+//& ------------------------------------------------------------------------------------------------
+
+// function formatter(a, b) {
+//     return a+b
+// }
+// function reformat(msg, callback) {
+//     const first = callback(msg, ".");
+//     const second = callback(first, ".");
+//     const third = callback(second, ".");
+//     return `TEXTIO: ${third}`;
+// }
+// console.log(reformat("hello", formatter));
+
+//& ------------------------------------------------------------------------------------------------
+
+function createContact(phoneNumber, name = "Anonymous", avatar = "default.jpg") {
+    if (phoneNumber == null) {
+        return "invalid phone number"
+    } else {
+        avatar = "/public/pictures/" + avatar;
+    }
+    return `Contact saved! Name: ${name}, Phone number: ${phoneNumber}, Avatar: ${avatar}`
+
+}
+
+console.log(createContact());
+
+
