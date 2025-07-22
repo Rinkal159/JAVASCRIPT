6bbcd0d3014a562c05a31cbad0a0b6dd74c68712
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-22 22:07:39 +0530

    Immediately Invoked Function Expression.

--- IIFE.js
+++ IIFE.js
@@ -1,36 +1,54 @@
-// IIFE(Immediately Invoked Function Expression)
-// --------------------------------------------------------------------------------------------------------------------------------------
-{
-    for (let i = 0; i <= 3; i++) {
-
-    // const print = () => console.log(`hello world! ${i}`);
-    // print();
-
-    (function() {
-        // console.log(`hellow world! : ${i}`);
-
-    })();
-    }
-}
+//* IIFE(Immediately Invoked Function Expression)
+//& --------------------------------------------------------------------------------------------------------------------------------------
 
 
 {
+    let a = 12;
     function sum(a, b) {
         return a+b;
     }
-    let sumthis = sum(10, 10);
-    sumthis;
+    
+    module.exports = {sum, a};
+}
 
+
+
+{
+    // for (let i = 0; i <= 3; i++) {
+
+    // // const print = () => console.log(`hello world! ${i}`);
+    // // print();
+
+    // (function() {
+    //     // console.log(`hellow world! : ${i}`);
+
+    // })();
+    // }
+}
+
+
+{
+    // let a = 12;
+    // let b = 12;
+
+    // function sum(x, y) {
+    //     return x + y;
+    // }
+
+    // module.exports = {
+    //     a, b, sum
+    // }
+    
 }
 {
-    const a = 12;
-    const b = 14;
-    const IIFE = ((...num) => {
-        const sum = num.reduce((acc, value) => acc + value, 0)
-        return sum;
+    // const a = 12;
+    // const b = 14;
+    // const IIFE = ((...num) => {
+    //     const sum = num.reduce((acc, value) => acc + value, 0)
+    //     return sum;
 
 
-    })(23, 45, 56, 78, 100);
+    // })(23, 45, 56, 78, 100);
 
     // console.log(IIFE);
 }
@@ -80,13 +98,13 @@
 
 
 {
-    let a = 4;
+    // let a = 4;
 
-    const IIFE = ((b) => {
-        return a+b;
-    })( 5)
+    // const IIFE = ((b) => {
+    //     return a+b;
+    // })( 5)
 
-    console.log(IIFE);
+    // console.log(IIFE);
 }
 
 
