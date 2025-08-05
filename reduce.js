ee540ac92abb4ecb176beb7b4840330154b86273
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-05 09:30:53 +0530

    Diving deep into reduce, more & more problems.

--- reduce.js
+++ reduce.js
@@ -194,9 +194,88 @@
         { user: "charlie", activity: 13 }
     ]
 
-    const mostActiveNum = userActivity.reduce((acc, curr) =>  curr.activity > acc.activity ? curr : acc);
+    const mostActiveNum = userActivity.reduce((acc, curr) => curr.activity > acc.activity ? curr : acc);
 
-    console.log(mostActiveNum);
-    
+    // console.log(mostActiveNum);
 
+
+}
+
+{
+    const expenses = [
+        { description: "groceries", amount: 50, category: "food" },
+        { description: "electricity bill", amount: 100, category: "utilities" },
+        { description: "dinner", amount: 30, category: "food" },
+        { description: "internet bill", amount: 50, category: "utilities" },
+        { description: "internet bill", amount: 50, category: "food" },
+    ];
+
+    const totalExpenses = expenses.reduce((acc, curr) => {
+        acc[curr.category] = acc[curr.category] ? acc[curr.category] + curr.amount : curr.amount;
+        return acc;
+    }, {})
+
+    console.log(totalExpenses);
+}
+
+{
+    const obj = [
+        { description: "write report", completed: "false", priority: 2 },
+        { description: "send email", completed: "true", priority: 3 },
+        { description: "prepare presentation", completed: "false", priority: 1 },
+        { description: "learn english", completed: "true", priority: 4 },
+    ];
+
+    const nonCompletedTasks = obj.reduce((acc, curr) => {
+        if (curr.completed === "false") {
+            acc[curr.priority] = curr.description;
+        }
+        return acc;
+    }, {});
+
+    console.log(nonCompletedTasks);
+}
+
+{
+    //* reduce + map 
+    const movieRatings = [
+        { title: "A", ratings: [4, 5, 3] },
+        { title: "B", ratings: [5, 5, 4] },
+        { title: "C", ratings: [3, 4, 2] },
+    ];
+
+    const averageRating = movieRatings.map((val) => {
+        const avg = val.ratings.reduce((acc, curr, i) => {
+            acc += curr;
+            return i == val.ratings.length - 1 ? acc / val.ratings.length : acc;
+        });
+
+        return { title: val.title, average: avg.toFixed(2) };
+        /*[
+            { title: 'A', ratings: '4.00' },
+            { title: 'B', ratings: '4.67' },
+            { title: 'C', ratings: '3.00' }
+        ]*/
+
+        val.ratings = avg.toFixed(2);
+        return val;
+        /*[
+            { title: 'A', ratings: '4.00' },
+            { title: 'B', ratings: '4.67' },
+            { title: 'C', ratings: '3.00' }
+        ]*/
+
+        return avg; // [ '4.00', '4.67', '3.00' ]
+
+    });
+
+    console.log(averageRating);
+
+    // when returning avg
+    const alltogether = movieRatings.reduce((acc, curr, i) => {
+        acc[curr.title] = averageRating[i];
+        return acc;
+    }, {})
+
+    // console.log(alltogether);
 }
