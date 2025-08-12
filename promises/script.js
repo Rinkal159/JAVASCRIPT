440c00c999a5e907784582fcd1ad13122b1aadb7
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-12 16:46:18 +0530

    More code on promises.

--- promises/script.js
+++ promises/script.js
@@ -1,4 +1,37 @@
 {
+    const divide = (n1, n2) => {
+        return new Promise((resolve, reject) => {
+            if (n2 === 0) {
+                reject("Cannot divide by zero");
+            } else {
+                resolve(`${n1}/${n2} = ${n1 / n2}`);
+            }
+        })
+    }
+
+    divide(0, 10)
+        .then(console.log)
+        .catch(console.log)
+}
+
+{
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("PROMISE")
+        }, 2000);
+    })
+
+    setTimeout(() => {
+        console.log("NORMAL");
+    }, 2000);
+
+    promise.then(console.log)
+
+    // Promise will execute first even though it is defined after setTimeout, because it is in micro task queue which has higher priority.
+    // but if a function returns a promise, then setTimeout will execute first when both expires at same time and function called after setTimeout (promise will execute first if function called before setTimeout).
+}
+
+{
     let num = 10;
     new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -23,64 +56,66 @@
         })
         .catch(() => console.log("something went wrong")
         )
+        .finally(() => console.log("finally block")
+        )
 }
 
 {
     let myPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("promise executed");
+            // console.log("promise executed");
             resolve("promise consumed")
         }, 1000);
     })
 
     myPromise.then((msg) => {
-        console.log(msg);
+        // console.log(msg);
 
     })
 }
 
 {
-    // let age = 1;
-    // let myPromiseTwo = new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         if (age > 15) {
-    //             resolve("age is greater than 15");
-    //         } else {
-    //             reject("age is less than 15")
-    //         }
-    //     }, 1000);
-    // })
-    // myPromiseTwo
-    //     .then((msg) => {
-    //         console.log(msg);
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
+    let age = 1;
+    let myPromiseTwo = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age > 15) {
+                resolve("age is greater than 15");
+            } else {
+                reject("age is less than 15")
+            }
+        }, 1000);
+    })
+    myPromiseTwo
+        .then((msg) => {
+            // console.log(msg);
+        })
+        .catch((err) => {
+            // console.log(err);
 
-    //     })
+        })
 }
 
 {
-    // new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve({ name: "rinkal", age: 17, degree: "bca" })
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ name: "rinkal", age: 17, degree: "bca" })
 
-    //     }, 1000);
-    // }).then((obj) => {
-    //     console.log(obj);
+        }, 1000);
+    }).then((obj) => {
+        // console.log(obj);
 
-    // })
+    })
 }
 
 {
     function namaste() {
-        console.log("hello");
-        
+        console.log("namaste");
+
     }
     function hello(n1) {
         n1();
     }
-    hello(namaste)
+    // hello(namaste)
 }
 
 {
@@ -115,16 +150,16 @@
     // function print(a, b, callback) {
     //     console.log(a, b);
     //     console.log(callback(a, b));
-        
+
     // }
 
     // function sum(a, b) {
     //     console.log(a+b);
-        
+
     // }
 
     // print(12, 13, sum)
-    
+
 }
 
 {
